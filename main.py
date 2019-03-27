0a178269be5ca8b63cd29ab25f3adfe254627991
liam oorts <liam.oorts@aloxy.io>
2019-03-27 20:29:49 +0100

    working basics

--- main.py
+++ main.py
@@ -4,9 +4,22 @@ from slackclient import SlackClient
 config = configparser.ConfigParser()
 config.read('init.ini')
 
-SLACK_BOT_TOKEN=config.get('SLACK_BOT_TOKEN')
+SLACK_BOT_TOKEN = str(config.get('slackbot', 'SLACK_BOT_TOKEN'))
 
-slackbot = SlackClient(str(SLACK_BOT_TOKEN))
+slackbot = SlackClient(SLACK_BOT_TOKEN)
 
-slackbot.send_message(slack_channel, message)
+slackbot.rtm_connect(with_team_state=False)
+
+message = "go fuck yourself"
+channel = "DHCCP42P6"
+
+events = slackbot.rtm_read()
+
+print(events)
+
+# print(slackbot.api_call("chat.postMessage", as_user="true", channel=channel, text=message))
+
+# slackbot.rtm_send_message('random', 'am testing')
+
+# slackbot.send_message(slack_channel, message)
 
