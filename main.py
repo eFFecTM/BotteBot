b0f002bc44dc5d7be96da0bea5289b027d0b9150
liam oorts <liam.oorts@aloxy.io>
2019-03-27 21:51:01 +0100

    wow. so much progress

--- main.py
+++ main.py
@@ -4,15 +4,33 @@ import time
 from slackclient import SlackClient
 
 
+def check_random_keywords(user_name, text_received, channel):
+    """To check for words used in normal conversation, adding instults and gifs/images"""
+    pass
+
+
+def check_general_keywords(user_name, text_received, channel):
+    """Check for serious shit. Predefined commands etc."""
+    pass
+
+
+def mention_question(user_name, text_received, channel):
+    """bot got mentioned or pm'd, answer the question"""
+    check_general_keywords(user_name, text_received, channel)
+    message = "hello " + user_name + ", you rule"
+    slackbot.api_call("chat.postMessage", as_user="true", channel=channel, text=message)
+
+
 def parse(events):
     for event in events:
         if event['type'] == 'message' and not "subtype" in event:
             user_id, text_received, channel = event['user'], event['text'], event['channel']
             if user_id != bot_id:
-                if ('@{}'.format(bot_id) in text_received) or (channel not in public_channels):
-                    user_name = slackbot.api_call("users.info", user=user_id)["user"]["name"]
-                    message = "hello " + user_name + ", you rule"
-                    slackbot.api_call("chat.postMessage", as_user="true", channel=channel, text=message)
+                user_name = slackbot.api_call("users.info", user=user_id)["user"]["name"]
+                if ('@{}'.format(bot_id) in text_received) or (channel not in public_channel_ids):
+                    mention_question(user_name, text_received, channel)
+                else:
+                    check_random_keywords(user_name, text_received, channel)
 
 
 config = configparser.ConfigParser()
@@ -25,7 +43,7 @@ slackbot = SlackClient(SLACK_BOT_TOKEN)
 slackbot.rtm_connect(with_team_state=False)
 
 bot_id = slackbot.api_call("auth.test")["user_id"]
-public_channels = slackbot.api_call("channels.list")
+public_channel_ids = [element["id"] for element in slackbot.api_call("channels.list")["channels"]]
 
 while(True):
     parse(slackbot.rtm_read())
