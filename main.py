5451eef2066d7c16626c8c6496275fa0a8c37c8b
thomas <r8.thomas@hotmail.com>
2019-03-28 19:35:21 +0100

    Added OpenWeatherMap implementation (currently for Antwerp)

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import configparser
 import time
 import requests
-from weather import Weather, Unit
-
+import pyowm
 from slackclient import SlackClient
 
 insult_triggers = ["insult", "got em"]
@@ -32,6 +31,24 @@ def check_general_keywords(user_name, text_received, channel):
     pass
 
 
+def get_weather_message():
+    owm = pyowm.OWM(API_KEY)
+    location = 'Antwerp'
+    observation = owm.weather_at_place(location)
+    w = observation.get_weather()
+    status = w.get_detailed_status()
+    temperature = w.get_temperature('celsius')
+    wind = w.get_wind()
+    rain = w.get_rain()
+    sunrise = w.get_sunrise_time('iso')
+    sunset = w.get_sunset_time('iso')
+
+    msg = "In {}, it is currently {}°C (min = {}°C, max = {}°C)\nStatus: {}\nWind: {} km/h\nSunrise is at {}, " \
+          "sunset is at {}. ".format(location, temperature['temp'], temperature['temp_min'], temperature['temp_max'],
+                                      status, wind['speed'], sunrise, sunset)
+    return msg
+
+
 def mention_question(user_name, text_received, channel):
     """bot got mentioned or pm'd, answer the question"""
     check_general_keywords(user_name, text_received, channel)
@@ -39,14 +56,8 @@ def mention_question(user_name, text_received, channel):
     slackbot.api_call("chat.postMessage", as_user="true", channel=channel, text=message)
 
     if 'weather' in text_received or 'forecast' in text_received:
-        # weather = Weather(unit=Unit.CELSIUS)
-        # location = weather.lookup_by_location('antwerp')
-        # condition = location.condition
-        # print(condition.text)
-        # message = 'Currently: ' + condition.text
-        message = "There will be rainbows tomorrow!"
-        # TODO Use https://bigl.es/using-python-to-get-weather-data/
-        slackbot.api_call("chat.postMessage", as_user="true", channel=channel, text=message)
+        message = get_weather_message()
+        send_message(message, channel)
 
 
 def parse(events):
@@ -61,19 +72,22 @@ def parse(events):
                     check_random_keywords(user_name, text_received, channel)
 
 
+# Read config file
 config = configparser.ConfigParser()
 config.read('init.ini')
-
 SLACK_BOT_TOKEN = str(config.get('slackbot', 'SLACK_BOT_TOKEN'))
+API_KEY = str(config.get('open_weather_map', 'API_KEY'))
 
+# Connect to Slack
 slackbot = SlackClient(SLACK_BOT_TOKEN)
-
 slackbot.rtm_connect(with_team_state=False)
 
+# Get all user IDs and channel IDs
 bot_id = slackbot.api_call("auth.test")["user_id"]
 user_ids = [element["id"] for element in slackbot.api_call("users.list")["members"]]
 public_channel_ids = [element["id"] for element in slackbot.api_call("channels.list")["channels"]]
 
+# Keep checking for incoming mentions or keywords
 while True:
     parse(slackbot.rtm_read())
     time.sleep(1)
