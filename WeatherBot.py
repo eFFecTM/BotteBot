780875f5d706007d78ce7a501b272ccac764992d
Jan De Laet <jan.delaet@hotmail.com>
2019-09-25 19:20:08 +0200

    Actually NLTK is not used, removing it, as well as the unused get location method. Cleaning up checked in files to sync with gitignore.

--- WeatherBot.py
+++ WeatherBot.py
@@ -1,12 +1,10 @@
 import pyowm
-from nltk import word_tokenize, pos_tag, ne_chunk, Tree
 
 
 def get_weather_message(words_received, api_key):
     """Get the current weather at a given location. Default location is Antwerp."""
     owm = pyowm.OWM(api_key)
     location = None
-    # location = get_location(words_received)  # Location API is not working anymore
     if location is None:
         location = 'Antwerp'  # Default location
     observation = owm.weather_at_place(location)
@@ -21,22 +19,3 @@ def get_weather_message(words_received, api_key):
           "Sunrise :sunrise: is at {}, sunset :city_sunset: is at {}. ".format(location, status, temperature['temp'],
             temperature['temp_min'], temperature['temp_max'], wind['speed'], sunrise, sunset)
     return msg
-
-
-def get_location(text):
-    chunked = ne_chunk(pos_tag(word_tokenize(text)))
-    continuous_chunk = []
-    current_chunk = []
-
-    for subtree in chunked:
-        if type(subtree) == Tree and subtree.label() == 'GPE':
-            current_chunk.append(" ".join([token for token, pos in subtree.leaves()]))
-        elif current_chunk:
-            named_entity = " ".join(current_chunk)
-            if named_entity not in continuous_chunk:
-                continuous_chunk.append(named_entity)
-                current_chunk = []
-        else:
-            continue
-
-    return continuous_chunk[0]
